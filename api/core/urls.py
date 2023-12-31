ba30acd02d7e9818060e1716cdc29a7b12b9ce85
stpalamar <stanislav6121@gmail.com>
2023-12-31 02:51:31 +0200

    fix urls

--- api/core/urls.py
+++ api/core/urls.py
@@ -5,9 +5,9 @@ from core.views import CreateUserView, LoginView, ManageUserView
 app_name = 'core'
 
 urlpatterns = [
-    path("create/", CreateUserView.as_view(), name="create"),
-    path('profile/', ManageUserView.as_view(), name='profile'),
-    path('login/', LoginView.as_view(), name='knox_login'),
-    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
-    path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
+    path("create", CreateUserView.as_view(), name="create"),
+    path('profile', ManageUserView.as_view(), name='profile'),
+    path('login', LoginView.as_view(), name='knox_login'),
+    path('logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('logoutall', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
 ]
